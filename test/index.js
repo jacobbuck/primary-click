2fae677393b7b7db94593584d318a517306353ef
Jacob Buck <buck.jacob@gmail.com>
2017-07-14 20:25:41 +1200

    Fix bugs, update tests

--- test/index.js
+++ test/index.js
@@ -4,68 +4,71 @@ var onPrimaryClick = require("../lib").onPrimaryClick;
 
 describe("isPrimaryClick", function() {
   it("returns false when any modifier key is pressed", function() {
-    expect(isPrimaryClick({ altKey: true, buttons: 1 })).toBe(false);
-    expect(isPrimaryClick({ ctrlKey: true, buttons: 1 })).toBe(false);
-    expect(isPrimaryClick({ metaKey: true, buttons: 1 })).toBe(false);
-    expect(isPrimaryClick({ shiftKey: true, buttons: 1 })).toBe(false);
+    expect(isPrimaryClick({ altKey: true, button: 0 })).toBe(false);
+    expect(isPrimaryClick({ ctrlKey: true, button: 0 })).toBe(false);
+    expect(isPrimaryClick({ metaKey: true, button: 0 })).toBe(false);
+    expect(isPrimaryClick({ shiftKey: true, button: 0 })).toBe(false);
   });
 
-  it("returns true when buttons is 1", function() {
-    expect(isPrimaryClick({ buttons: 1 })).toBe(true);
-  });
-
-  it("returns true when button is 0 if buttons is unset", function() {
+  it("returns true when button is 0", function() {
     expect(isPrimaryClick({ button: 0 })).toBe(true);
   });
 
-  it("returns false when buttons is not 1", function() {
-    expect(isPrimaryClick({ buttons: -1 })).toBe(false);
-    expect(isPrimaryClick({ buttons: 2 })).toBe(false);
-    expect(isPrimaryClick({ buttons: false })).toBe(false);
-    expect(isPrimaryClick({ buttons: "hello" })).toBe(false);
+  it("returns true when button is 0 if buttons is set", function() {
+    expect(isPrimaryClick({ button: 0, buttons: 0 })).toBe(true);
+    expect(isPrimaryClick({ button: 0, buttons: 1 })).toBe(true);
   });
 
-  it("returns false when button is not 0 if buttons is unset", function() {
-    expect(isPrimaryClick({ button: -1 })).toBe(false);
+  it("returns false when button is not 0", function() {
     expect(isPrimaryClick({ button: 1 })).toBe(false);
+    expect(isPrimaryClick({ button: 3 })).toBe(false);
+    expect(isPrimaryClick({ button: -1 })).toBe(false);
     expect(isPrimaryClick({ button: false })).toBe(false);
     expect(isPrimaryClick({ button: "hello" })).toBe(false);
   });
 
-  it("ignores button if buttons is present", function() {
-    expect(isPrimaryClick({ buttons: 0, button: 0 })).toBe(false);
-    expect(isPrimaryClick({ buttons: 3, button: 0 })).toBe(false);
-    expect(isPrimaryClick({ buttons: 5, button: 0 })).toBe(false);
-    expect(isPrimaryClick({ buttons: 7, button: 0 })).toBe(false);
+  it("returns false when buttons is not 0 or 1", function() {
+    expect(isPrimaryClick({ button: 0, buttons: 2 })).toBe(false);
+    expect(isPrimaryClick({ button: 0, buttons: 8 })).toBe(false);
+    expect(isPrimaryClick({ button: 0, buttons: -1 })).toBe(false);
+    expect(isPrimaryClick({ button: 0, buttons: false })).toBe(false);
+    expect(isPrimaryClick({ button: 0, buttons: "hello" })).toBe(false);
+  });
+
+  it("uses which if button is not present", function() {
+    expect(isPrimaryClick({ which: 0 })).toBe(false);
+    expect(isPrimaryClick({ which: 1 })).toBe(true);
+    expect(isPrimaryClick({ which: 2 })).toBe(false);
+    expect(isPrimaryClick({ which: 3 })).toBe(false);
   });
 });
 
 describe("onPrimaryClick", function() {
   it("calls fn if isPrimaryClick returns true", function() {
     var spy = expect.createSpy();
-    onPrimaryClick(spy)({ buttons: 1 });
+    onPrimaryClick(spy)({ button: 0 });
     expect(spy).toHaveBeenCalled();
   });
 
   it("doesn't call fn if isPrimaryClick returns false", function() {
     var spy = expect.createSpy();
-    onPrimaryClick(spy)({ ctrlKey: true, buttons: 2 });
+    onPrimaryClick(spy)({ ctrlKey: true, button: 1 });
     expect(spy).toNotHaveBeenCalled();
   });
 
   it("returns the value fn returns if called", function() {
     var spy = expect.createSpy().andReturn("lol");
-    expect(onPrimaryClick(spy)({ buttons: 1 })).toBe("lol");
+    expect(onPrimaryClick(spy)({ button: 0 })).toBe("lol");
   });
 
   it("returns `true` if fn doesn't get called", function() {
     var spy = expect.createSpy().andReturn("lol");
-    expect(onPrimaryClick(spy)({ ctrlKey: true, buttons: 2 })).toBe(true);
+    expect(onPrimaryClick(spy)({ ctrlKey: true, button: 1 })).toBe(true);
   });
 
   it("passes down arguments to fn", function() {
     var spy = expect.createSpy();
-    var mockEvent = { buttons: 1 };
+    var mockEvent = { button: 0 };
     onPrimaryClick(spy)(mockEvent, "goodbye", 420);
     expect(spy).toHaveBeenCalledWith(mockEvent, "goodbye", 420);
   });
