e9b34037fdb5948a4a6b8ab9e94cbcd8a758ef16
Jacob Buck <buck.jacob@gmail.com>
2021-05-09 00:55:39 +0000

    Update rollup.config.js to read properties from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [
-    { file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true },
-    { file: 'lib/index.esm.js', format: 'esm', sourcemap: true },
+    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.module, format: 'esm', sourcemap: true },
   ],
   plugins: [babel({ babelHelpers: 'bundled' })],
 };
